e92e029070b7444d5a2b19d3a94486536d5adf29
Tan Wang <timmy940408@me.com>
2016-09-18 18:53:17 -0400

    The free version can now display interstitial ad

--- app/src/free/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/free/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.support.v4.util.Pair;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
@@ -12,15 +13,35 @@ import android.view.MenuItem;
 import android.view.View;
 
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
 import com.tanwang9408.jokemodule.JokeActivity;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    private InterstitialAd mInterstitial;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loadInterstitial();
+
+
+
+    }
+
+    private void loadInterstitial(){
+        mInterstitial=new InterstitialAd(this);
+        mInterstitial.setAdUnitId(this.getString(R.string.interstitial_ad_unit_id));
+
+
+
+        AdRequest adRequest=new AdRequest.Builder().build();
+        mInterstitial.loadAd(adRequest);
+
+
     }
 
 
@@ -46,13 +67,26 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    private void showInterstitial(){
+        if(mInterstitial.isLoaded()){
+            mInterstitial.show();
+
+        }
+
+
+
+    }
+
     public void tellJoke(View view) {
 
+
+        showInterstitial();
         EndpointsAsyncTask task=new EndpointsAsyncTask();
         task.execute(this);
         if(task.getStatus()== AsyncTask.Status.FINISHED) {
 
-            Log.e("111", task.result);
+
+
         }
 
 
